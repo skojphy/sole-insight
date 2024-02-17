ec8a8f430316765e08e29c2ec13976683bc093a3
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2024-02-17 23:31:32 +0900

    :truck: Server 파일 이동

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { webSocketServer } from './webSocket.js';
+import { webSocketServer } from './server/webSocket.js';
 
 export default defineConfig({
 	server: {
